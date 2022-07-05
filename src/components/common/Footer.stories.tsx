1f3b8de3bb57662aaddb32d93c1d16373e7b7347
Cut0 <zeron9421@gmail.com>
2022-07-05 16:39:27 +0900

    feat: ディレクトリ構造の変更と storybook の修正

---
+++ src/components/common/Footer.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { Footer } from './Footer';
+
+const meta: ComponentMeta<typeof Footer> = {
+  component: Footer,
+};
+export default meta;
+
+export const Default: ComponentStoryObj<typeof Footer> = {};
