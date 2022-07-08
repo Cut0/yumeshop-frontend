019f7d3425612f3f5cc89034100ed956134c4c13
Cut0 <zeron9421@gmail.com>
2022-07-08 13:38:24 +0900

    feat: アイコンの追加

---
+++ src/components/common/Icons/ArrowLeftSvgIcon.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { ArrowLeftSvgIcon } from './ArrowLeftSvgIcon';
+
+const meta: ComponentMeta<typeof ArrowLeftSvgIcon> = {
+  component: ArrowLeftSvgIcon,
+};
+export default meta;
+
+export const Default: ComponentStoryObj<typeof ArrowLeftSvgIcon> = {};
