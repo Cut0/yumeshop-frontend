bb3412bfb22fe68927abea60de196d19e1e246df
Cut0 <zeron9421@gmail.com>
2022-07-08 13:40:57 +0900

    feat: Top ページの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,58 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { css } from '@emotion/react';
-import { Container, Main, Title, TokenTest } from 'src/styles/Home';
+import { Caruosel } from 'src/components/common/Carousel';
+import { useShopItems } from '../features/shopItems/ShopItemsHooks';
 
-const Home: NextPage = () => (
-  <Container>
-    <Head>
-      <title>Yumeshop</title>
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage = () => {
+  const shopItems = useShopItems({});
 
-    <Main>
-      <Image src="/shopping-bag.jpg" alt="買い物袋" width={600} height={600} />
-
-      <TokenTest>
-        <Title
-          css={css`
-            background-color: red;
-          `}
-        >
-          Welcome to Yumeshop
-        </Title>
-      </TokenTest>
-    </Main>
-  </Container>
-);
+  return (
+    <main
+      css={css`
+        min-height: 100vh;
+        max-width: 1152px;
+        margin: 0 auto;
+      `}
+    >
+      <Head>
+        <title>Yumeshop</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        css={css`
+          max-width: 600px;
+          margin: 0 auto;
+        `}
+      >
+        <Caruosel>
+          {shopItems.data !== undefined &&
+            shopItems.data
+              .map(({ thumbnail }, index) => ({
+                key: index,
+                src: thumbnail,
+              }))
+              .map(({ key, src }) => (
+                <div
+                  css={css`
+                    display: inline-block;
+                    width: 100%;
+                  `}
+                >
+                  <Image
+                    key={key}
+                    src={src}
+                    width="300"
+                    height="300"
+                    layout="responsive"
+                  />
+                </div>
+              ))}
+        </Caruosel>
+      </div>
+    </main>
+  );
+};
 
 export default Home;
